65e813a02e7412802dd7de3dddbb2dc212abb2d9
Marc Vidal <marcgavvid@gmail.com>
2024-06-19 09:35:17 -0400

    content changes

--- src/components/WhatsNext.js
+++ src/components/WhatsNext.js
@@ -26,16 +26,6 @@ function WhatsNext() {
       </Section>
       <Section>
         <h2 className="text-2xl font-bold mb-3">
-          Pro Bono CIBC Project
-        </h2>
-        <h3 className="text-1x mb-3">
-          Recently invited to collaborate with a team of 7 to create a project for MBA students at Western University, Ivey school of business, for CIBC. 
-          My team is currently in the planning stage. I look forward to not only acquire new skills, but to further them.
-          I am also eager to learn from my team of experienced and talented software developers. 
-        </h3>
-      </Section>
-      <Section>
-        <h2 className="text-2xl font-bold mb-3">
           BeSuccessful
         </h2>
         <h3 className="text-1x mb-3">
